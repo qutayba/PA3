0cd5798f7339f0fb0daf61b3eefd23acd1964dd8
Qutayba Ali <qutayba.ali@hva.nl>
2019-12-03 22:29:48 +0100

    Adding code comments

--- src/EncryptionManager.java
+++ src/EncryptionManager.java
@@ -12,47 +12,89 @@ public class EncryptionManager {
     private BigInteger e;
     private BigInteger d;
 
-
+    /**
+     * Calculates the p and q values
+     * @param nValue The n value to be used in the calculation
+     * @throws Exception Throws exception with any calculation error
+     */
     public void calculatePAndQ(BigInteger nValue) throws Exception {
+
+        // Assigning the n value with start prime number for p
         n = nValue;
         p = INIT_PRIM_NUMBER;
 
+        // Checking the result of dividing the p and q values
+        // will result the n value
         while (p.compareTo(n.divide(INIT_PRIM_NUMBER)) <= 0) {
 
+            // If the result equals the n value, than assign
+            // the tested value to q
             if(n.mod(p).equals(BigInteger.ZERO)) {
                 q = n.divide(p);
                 return;
             }
+
+            // Otherwise, find the next prime number and repeat the test
             p = p.nextProbablePrime();
         }
 
         throw new Exception("No solution found");
     }
 
+
+    /**
+     * Calculates the e value based on the p and q (from step 1)
+     * @throws Exception Throws exception with any calculation error
+     */
     public void calculateE() throws Exception {
+
         if(p == null || q == null) {
             throw new Exception("No q or p calculated. You need to execute step 1.");
         }
 
+        // Getting the phi of the two prime-numbers (q and p)
         BigInteger phi = getPhi();
-        e = BigInteger.valueOf(3L);
+
+        // Starting will the first start prime number
+        e = INIT_PRIM_NUMBER;
+
+        // Checking if the greatest common divisor equals one
+        // otherwise, increase the e value with one and test again
         while(!phi.gcd(e).equals(BigInteger.ONE))
         {
             e = e.add(BigInteger.ONE);
         }
     }
 
+    /**
+     * Calculates the value of d
+     * @param nValue The value of n
+     * @param eValue The value of e
+     * @throws Exception Throws exception with any calculation error
+     */
     public void calculateD(BigInteger nValue, BigInteger eValue) throws Exception {
         if(nValue == null || eValue == null) {
             throw new Exception("No value for n or e can be found.");
         }
 
+        // Assigning the entered values
         n = nValue;
         e = eValue;
+
+        // Calculating the p and q values based on the entered n
         calculatePAndQ(n);
+
+        // Calculating the d value by using mod-inverse (see the manual for more info)
         d = e.modInverse(getPhi());
     }
 
+
+    /**
+     * Encrypts a message value (only numeric)
+     * @param message The message value to be encrypted
+     * @return The encrypted message
+     * @throws Exception Throws exception with any encryption error
+     */
     public String encrypt(String message) throws Exception {
         if(message == null || message.isEmpty()) {
             throw  new Exception("There is no message entered.");
@@ -62,15 +104,23 @@ public class EncryptionManager {
             throw new Exception("No n or e value found. You need to execute step 1 and 2.");
         }
 
+        // Convening the string into big integer
         BigInteger m = new BigInteger(message);
         if(m.compareTo(n) > 0) {
             throw new Exception("The value to be encrypted is greater than the n value. Please, change the values and try again.");
         }
 
+        // Encrypting the message based on the calculated e and n values
         BigInteger c = m.modPow(e, n);
         return c.toString();
     }
 
+    /**
+     * Decrypts a message value
+     * @param message The message to be decrypted
+     * @return The decrypted message
+     * @throws Exception Throws exception with any decryption error
+     */
     public String decrypt(String message) throws Exception {
         if(message == null || message.isEmpty()) {
             throw  new Exception("There is no message entered.");
@@ -80,6 +130,7 @@ public class EncryptionManager {
             throw new Exception("No n or d value found. You need to execute step 1.");
         }
 
+        // Decrypting the message based on the calculated d and n values
         BigInteger dm = new BigInteger(message).modPow(d, n);
         return dm.toString();
     }
@@ -101,6 +152,10 @@ public class EncryptionManager {
         return d;
     }
 
+    /**
+     * Calculates the phi value based on p and q
+     * @return The phi value
+     */
     public BigInteger getPhi() {
         BigInteger pMin1 = p.subtract(BigInteger.ONE);
         BigInteger qMin1 = q.subtract(BigInteger.ONE);
